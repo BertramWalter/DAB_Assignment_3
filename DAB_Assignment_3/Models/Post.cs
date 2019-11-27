9f9819465b7fcab34e1fdddb6a627f69c11c7b96
Bertram <bw-sp@live.dk>
2019-11-27 15:16:17 +0100

    Added attributes to circle, post and users models (Deleted blocked model)

--- DAB_Assignment_3/Models/Post.cs
+++ DAB_Assignment_3/Models/Post.cs
@@ -1,11 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace DAB_Assignment_3.Models
 {
-    class Post
+    public class Post
     {
-        public int PostId { get; set; }
+        public Post()
+        {
+            CommentsOnPost = new List<string>();
+            PostCreationTime = DateTime.Now;
+            PostedInCircle = "";
+        }
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string PostId { get; set; }
+
+        [BsonElement("ContentType")] 
+        public string ContentType { get; set; }
+
+        [BsonElement("Content")]
+        public string Content { get; set; }
+        //Dictionary?????? Binding Type and content together?
+
+        [BsonElement("PostedInCircle")]
+        public string PostedInCircle { get; set; }
+
+        [BsonElement("AuthorOfPost")]
+        public string AuthorOfPost { get; set; }
+
+        [BsonElement("PostCreationTime")]
+        public DateTime PostCreationTime { get; set; }
+
+        [BsonElement("CommentsOnPost")]
+        public List<string> CommentsOnPost { get; set; }
     }
 }
