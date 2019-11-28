43a0a900d246a218d446a3cea57047cb3697e107
Bertram <bw-sp@live.dk>
2019-11-28 11:27:27 +0100

    Commit for pull

--- DAB_Assignment_3/Models/Circle.cs
+++ DAB_Assignment_3/Models/Circle.cs
@@ -10,7 +10,7 @@ namespace DAB_Assignment_3.Models
     {
         public Circle()
         {
-            UsersInCircle = new List<string>();
+           // UsersInCircle = new List<string>();
         }
 
         [BsonId]
@@ -20,8 +20,8 @@ namespace DAB_Assignment_3.Models
         [BsonElement("NameOfCircle")]
         public string Name { get; set; }
 
-        [BsonElement("UsersInCircle")]
-        public List<string> UsersInCircle { get; set; }
+        [BsonElement("UserId")]
+        public List<string> UserId { get; set; }
 
     }
 }
