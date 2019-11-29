a8afac0e2a50d843b4adad13baa4b9d304694bf0
Marcus Bech <43644925+morogfar21@users.noreply.github.com>
2019-11-29 11:09:53 +0100

    Changed datetime dummydata

--- DAB_Assignment_3/Services/DummyData.cs
+++ DAB_Assignment_3/Services/DummyData.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using DAB_Assignment_3.Models;
+using Microsoft.VisualBasic;
 using MongoDB.Driver;
 
 namespace DAB_Assignment_3.Services
@@ -120,16 +121,16 @@ namespace DAB_Assignment_3.Services
         public static void InsertCommentDummyData(IMongoDatabase database)
         {
             var comments = database.GetCollection<Comment>("Comments");
-            var comment1 = new Comment(postid:"1",authorid: "a1", authorname:"Per Thorsen",commentstring:"Hvor er du?",DateTime.Now );
-            var comment2 = new Comment(postid: "1", authorid: "a2", authorname: "Poul E", commentstring: "WebAPI load", DateTime.Now);
-            var comment3 = new Comment(postid: "3", authorid: "a3", authorname: "Henrik", commentstring: "MongoDb virker ikke", DateTime.Now);
-            var comment4 = new Comment(postid: "3", authorid: "a3", authorname: "Henrik", commentstring: "Nu virker det!", DateTime.Now);
-            var comment5 = new Comment(postid: "4", authorid: "a4", authorname: "Søren H", commentstring: "Giver det mening??", DateTime.Now);
-            var comment6 = new Comment(postid: "5", authorid: "a5", authorname: "Lars M", commentstring: "Nu er jeg vaagen", DateTime.Now);
-            var comment7 = new Comment(postid: "5", authorid: "a6", authorname: "Henrik O", commentstring: "Du kan bare komme forbi kontoret", DateTime.Now);
-            var comment8 = new Comment(postid: "6", authorid: "a7", authorname: "Torben", commentstring: "Jeg tager papir med", DateTime.Now);
-            var comment9 = new Comment(postid: "7", authorid: "a8", authorname: "Kim", commentstring: "Kravsspec og review", DateTime.Now);
-            var comment10 = new Comment(postid: "7", authorid: "a9", authorname: "Frankster", commentstring: "ehhhmm", DateTime.Now);
+            var comment1 = new Comment(postid:"1",authorid: "a1", authorname:"Per Thorsen",commentstring:"Hvor er du?", new DateTime(year: 2019, month: 11, day: 29, hour: 19, minute: 3, second: 43));
+            var comment2 = new Comment(postid: "1", authorid: "a2", authorname: "Poul E", commentstring: "WebAPI load", new DateTime(year: 2019, month: 11, day: 29, hour: 11, minute: 23, second: 23));
+            var comment3 = new Comment(postid: "3", authorid: "a3", authorname: "Henrik", commentstring: "MongoDb virker ikke", new DateTime(year: 2019, month: 11, day: 29, hour: 11, minute: 23, second: 28));
+            var comment4 = new Comment(postid: "3", authorid: "a3", authorname: "Henrik", commentstring: "Nu virker det!", new DateTime(year: 2019, month: 11, day: 29, hour: 11, minute: 53, second: 16));
+            var comment5 = new Comment(postid: "4", authorid: "a4", authorname: "Søren H", commentstring: "Giver det mening??", new DateTime(year: 2019, month: 11, day: 29, hour: 7, minute: 23, second: 23));
+            var comment6 = new Comment(postid: "5", authorid: "a5", authorname: "Lars M", commentstring: "Nu er jeg vaagen", new DateTime(year: 2019, month: 11, day: 29, hour: 10, minute: 23, second: 23));
+            var comment7 = new Comment(postid: "5", authorid: "a6", authorname: "Henrik O", commentstring: "Du kan bare komme forbi kontoret", new DateTime(year: 2019, month: 11, day: 30, hour: 8, minute: 23, second: 23));
+            var comment8 = new Comment(postid: "6", authorid: "a7", authorname: "Torben", commentstring: "Jeg tager papir med", new DateTime(year: 2019, month: 11, day: 18, hour: 4, minute: 40, second: 33));
+            var comment9 = new Comment(postid: "7", authorid: "a8", authorname: "Kim", commentstring: "Kravsspec og review", new DateTime(year: 2019, month: 11, day: 29, hour: 10, minute: 20, second: 0));
+            var comment10 = new Comment(postid: "7", authorid: "a9", authorname: "Frankster", commentstring: "ehhhmm", new DateTime(year:2019,month:11,day:30,hour:10,minute:21,second:1));
             comments.InsertOne(comment1);
             comments.InsertOne(comment2);
             comments.InsertOne(comment3);
@@ -140,7 +141,7 @@ namespace DAB_Assignment_3.Services
             comments.InsertOne(comment8);
             comments.InsertOne(comment9);
             comments.InsertOne(comment10);
-            
+
             //comments.InsertMany(comment1,comment2, comment3,comment4,comment5,comment6,comment7,comment8,comment9,comment10]);
             /*comments.InsertMany(
                 [ < comment1 1 > , < comment2 2 >, ... ], 
